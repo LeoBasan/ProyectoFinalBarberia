a848d86c97caab3e1e985c1ed4d57dfa14477f7f
LeoBasan <leopoldobasanta@gmail.com>
2024-06-21 13:20:01 -0300

    Cambio de menu
    Manejo de menu de forma diferente para facilitar todo

--- src/main/java/controllers/MenuMainController.java
+++ src/main/java/controllers/MenuMainController.java
@@ -2,104 +2,131 @@ package controllers;
 
 import com.aplicacion.barbero.Barbero;
 import com.aplicacion.cliente.Cliente;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.control.MenuButton;
-import javafx.scene.control.MenuItem;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MenuMainController extends BaseController {
     private MenuBarberoController menuBarberoController;
     private MenuTurnosController menuTurnosController;
     private MenuClienteController menuClienteController;
 
+    private Map<Button,AnchorPane> buttonToFormMap= new HashMap<>();
     //Panel ppal
     @FXML
+    private StackPane stackPaneMenu;
+    @FXML
     private AnchorPane anchorPaneMenuMain;
-    //Conjunto panel de arriba y abajo
+    //Conjunto de paneles
     @FXML
     private BorderPane borderPaneMenu;
     //Panel de arriba
     @FXML
     private AnchorPane anchorPaneUp;
-
+    @FXML
+    private AnchorPane anchorPaneUp2;
     @FXML
     private ImageView companyLogo;
-
     @FXML
     private Label companyName;
-
+    //Panel izquierdo
     @FXML
-    private MenuButton profileButton;
-
+    private AnchorPane leftAnchorPane1;
     @FXML
-    private ImageView profileButtonImage;
-
+    private AnchorPane getLeftAnchorPane2;
     @FXML
-    private MenuItem profileOption1;
-
+    private Button historialButton;
     @FXML
-    private MenuItem profileOption2;
-
+    private Button addTurnoButton;
     @FXML
-    private StackPane stackPaneMenu;
+    private Button modifyProfileButton;
+    @FXML
+    private Button logOutButton;
     //Panel de abajo
     @FXML
-    private AnchorPane anchorPaneDown;
+    private AnchorPane anchorPaneCenter;
     //Paneles de escenas que voy a manejar abajo
     @FXML
     private AnchorPane menuClientes;
 
     @FXML
-    private AnchorPane menuBarbero;
+    private AnchorPane menuModPerfil;
 
     @FXML
     private AnchorPane menuTurnos;
 
+    //------------------------------------------------------------------
+    private Button selectedButton;//Almecena el boton seleccionado
     @FXML
     public void initialize(){
-        inicScenes();
+        buttonToFormMap.put(historialButton,menuClientes);
+        buttonToFormMap.put(addTurnoButton,menuTurnos);
+        buttonToFormMap.put(modifyProfileButton,menuModPerfil);
+        selectedButton= historialButton;
+        switchForm(new ActionEvent(historialButton,null));
+
+        //listeners para cada boton
+        historialButton.setOnAction(this::switchForm);
+        addTurnoButton.setOnAction(this::switchForm);
+        modifyProfileButton.setOnAction(this::switchForm);
+        logOutButton.setOnAction(event -> handleLogout());
+        //actualizo la botonera dependiendo el inicio
+        updateUIBasedOnUser();
+    }
+    public void switchForm(ActionEvent actionEvent){
+        //Obtengo el boton que disparo el evento
+        Button clickedButton=(Button) actionEvent.getSource();
+        selectedButton= clickedButton;
+        //Oculto todos los formularios
+        buttonToFormMap.values().forEach(form->form.setVisible(false));
+        //Mostrar formulario asociado al boton
+        AnchorPane selectedForm= buttonToFormMap.get(clickedButton);
+        if(selectedForm!=null){
+            selectedForm.setVisible(true);
+        }
+    }
+    private void updateUIBasedOnUser(){//depende en que user se loguea habilito agregar turno o no
+        if(!typeOfUser()){
+            addTurnoButton.setDisable(true);
+        }else {
+            addTurnoButton.setDisable(false);
+        }
     }
     public void setCliente(Cliente cliente){
         super.setCliente(cliente);
-        handleMainMenu();
     }
     public void setBarbero(Barbero barbero){
         super.setBarbero(barbero);
-        handleMainMenu();
-    }
-    private void handleMainMenu(){
-        if(typeOfUser()){
-            showMenuBarbero();
-        } else {
-            showMenuClientes();
-        }
     }
     private boolean typeOfUser(){//si viene de cliente retorna true sino false
-        System.out.println(getCliente());
-        return getCliente()==null;
+        return getCliente()!=null;
     }
-    private void inicScenes(){
-        menuClientes.setVisible(false);
-        menuBarbero.setVisible(false);
-        menuTurnos.setVisible(false);
-    }
-    private void showMenuClientes(){
-        menuBarbero.setVisible(false);
-        menuTurnos.setVisible(false);
-        menuClientes.setVisible(true);
-    }
-    private void showMenuBarbero(){
-        menuTurnos.setVisible(false);
-        menuClientes.setVisible(false);
-        menuBarbero.setVisible(true);
-    }
-    private void showMenuTurnos(){
-        menuBarbero.setVisible(false);
-        menuClientes.setVisible(false);
-        menuTurnos.setVisible(true);
+    private void handleLogout(){
+        try{
+            FXMLLoader loader= new FXMLLoader(getClass().getResource("/interfaz/login.fxml"));
+            Parent root= loader.load();
+            BaseController controller= loader.getController();
+            controller.setClienteRepository(getClienteRepository());
+            controller.setBarberoRepository(getBarberoRepository());
+
+            Stage stage= (Stage) logOutButton.getScene().getWindow();
+            stage.setScene(new Scene(root));
+            stage.show();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 }
---
+++ src/main/java/controllers/ModPerfilController.java
@@ -0,0 +1,42 @@
+package controllers;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+
+public class ModPerfilController {
+
+    @FXML
+    private TextField adressTextField;
+
+    @FXML
+    private AnchorPane anchorPane;
+
+    @FXML
+    private Button confirmButton;
+
+    @FXML
+    private ImageView icon1;
+
+    @FXML
+    private ImageView icon2;
+
+    @FXML
+    private ImageView icon3;
+
+    @FXML
+    private ImageView icon4;
+
+    @FXML
+    private TextField lastNameTextField;
+
+    @FXML
+    private TextField nameTextField;
+
+    @FXML
+    private TextField phoneTextField;
+
+}
+
